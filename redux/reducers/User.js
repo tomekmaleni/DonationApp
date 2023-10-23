cf22abfe5d610fdca9d42d680c2953855ce0ab59
Tomislav Matoković <tomekmaleni@gmail.com>
2023-10-23 14:07:18 +0200

    creating User slice

---
+++ redux/reducers/User.js
@@ -0,0 +1,21 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  firstName: 'Nata',
+  lastName: 'Vacheishvili',
+  userId: 1,
+};
+
+export const User = createSlice({
+  name: 'user',
+  initialState: initialState,
+  reducers: {
+    updateFirstName: (state, action) => {
+      state.firstName = action.payload.firstName;
+    },
+  },
+});
+
+export const {updateFirstName} = User.actions;
+
+export default User;
