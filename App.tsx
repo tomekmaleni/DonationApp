a39a1ec1d7da8abced4b2eabe9143dadf1ab23b0
Tomislav Matoković <tomekmaleni@gmail.com>
2023-10-23 09:50:41 +0200

    adding Status bar color

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
 import MainNavigation from './navigation/MainNavigation';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={'#2979F2'} barStyle={'light-content'} />
       <MainNavigation />
     </NavigationContainer>
   );
