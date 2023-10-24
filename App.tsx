7098e3b93d1d7a6864ca211b6e27cccb3ac98799
Tomislav Matoković <tomekmaleni@gmail.com>
2023-10-24 13:49:56 +0200

    configuring Persisting Store

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 import MainNavigation from './navigation/MainNavigation';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import store, {persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar backgroundColor={'#2979F2'} barStyle={'light-content'} />
-        <MainNavigation />
-      </NavigationContainer>
+      <PersistGate persistor={persistor} loading={null}>
+        <NavigationContainer>
+          <StatusBar backgroundColor={'#2979F2'} barStyle={'light-content'} />
+          <MainNavigation />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- redux/store.js
+++ redux/store.js
@@ -1,17 +1,29 @@
-import {combineReducers} from '@reduxjs/toolkit';
-import {configureStore} from '@reduxjs/toolkit';
-// import {logger} from 'redux-logger';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+import {logger} from 'redux-logger';
 import User from './reducers/User';
 
 const rootReducer = combineReducers({
   user: User,
 });
 
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
-  // middleware: getDefaultMiddleware => {
-  //   return getDefaultMiddleware().concat(logger);
-  // },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger);
+  },
 });
 
 export default store;
+
+export const persistor = persistStore(store);
