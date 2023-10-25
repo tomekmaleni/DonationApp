c019008140674a1c0a4ebcb6e4a4c37b041fbb5b
Tomislav Matoković <tomekmaleni@gmail.com>
2023-10-25 11:20:31 +0200

    setting up "Reset to Initial state" reducer

--- redux/reducers/User.js
+++ redux/reducers/User.js
@@ -4,6 +4,8 @@ const initialState = {
   firstName: 'Nata',
   lastName: 'Vacheishvili',
   userId: 1,
+  profileImage:
+    'https://cdn.dribbble.com/users/1577045/screenshots/4914645/media/028d394ffb00cb7a4b2ef9915a384fd9.png?compress=1&resize=400x300&vertical=top',
 };
 
 export const User = createSlice({
@@ -13,9 +15,12 @@ export const User = createSlice({
     updateFirstName: (state, action) => {
       state.firstName = action.payload.firstName;
     },
+    resetToInitialState: () => {
+      return initialState;
+    },
   },
 });
 
-export const {updateFirstName} = User.actions;
+export const {updateFirstName, resetToInitialState} = User.actions;
 
 export default User.reducer;
